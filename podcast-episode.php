0e9cad819682f518de4253cf026f5603de88d70b
Victor Andersen <v@victorb.net>
2017-01-27 01:14:30 -0800

    meta layout and css update

--- podcast-episode.php
+++ podcast-episode.php
@@ -38,6 +38,17 @@
         </section>
         
         <footer>
+            <ul class="post_footer_meta text-muted list-inline">
             <?php the_tags('<p class="tags">', ' ', '</p>'); ?>
+              <?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : ?>
+        <li>
+            <?php
+                $sp = '<span class="glyphicon glyphicon-comment"></span> ';
+                comments_popup_link($sp . __( 'Leave a comment', "simple-bootstrap"), $sp . __( '1 Comment', "simple-bootstrap"), $sp . __( '% Comments', "simple-bootstrap"));
+            ?>
+        </li>
+        <?php endif; ?>
+        <?php edit_post_link(__( 'Edit', "simple-bootstrap"), '<li><span class="glyphicon glyphicon-pencil"></span> ', '</li>'); ?>
+            </ul>
         </footer>
 </article>
--- podcast-meta.php
+++ podcast-meta.php
@@ -18,21 +18,5 @@
                 <?php the_date(); ?>
             </a>
         </li>
-<!--
-        <li>
-            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID'));?>">
-                <span class="glyphicon glyphicon-user"></span>
-                <?php the_author(); ?>
-            </a>
-        </li>
--->
-        <?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : ?>
-        <li>
-            <?php
-                $sp = '<span class="glyphicon glyphicon-comment"></span> ';
-                comments_popup_link($sp . __( 'Leave a comment', "simple-bootstrap"), $sp . __( '1 Comment', "simple-bootstrap"), $sp . __( '% Comments', "simple-bootstrap"));
-            ?>
-        </li>
-        <?php endif; ?>
-        <?php edit_post_link(__( 'Edit', "simple-bootstrap"), '<li><span class="glyphicon glyphicon-pencil"></span> ', '</li>'); ?>
+      
     </ul>
