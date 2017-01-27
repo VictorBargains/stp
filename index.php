a31ae2b397cd0895668b31867a35a101f8dd7ecf
Victor Andersen <v@victorb.net>
2017-01-27 01:49:50 -0800

    adding custom index page and minor css tweaks

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+
+<div id="content" class="row">
+
+	<div id="main" class="<?php simple_boostrap_main_classes(); ?>" role="main">
+
+		<?php if (have_posts()) : ?>
+
+		<?php while (have_posts()) : the_post(); ?>
+		
+        <?php 
+            if( $post->post_type == 'podcast' ){
+                $multiple_on_page = true;
+                include(get_stylesheet_directory() . '/podcast-episode.php');
+            } else {
+                simple_boostrap_display_post(true);
+            }
+         ?>
+		
+		<?php endwhile; ?>	
+		
+		<?php simple_boostrap_page_navi(); ?>	
+		
+		<?php else : ?>
+		
+		<article id="post-not-found" class="block">
+		    <p><?php _e("No posts found.", "simple-bootstrap"); ?></p>
+		</article>
+		
+		<?php endif; ?>
+
+	</div>
+
+	<?php get_sidebar("left"); ?>
+	<?php get_sidebar("right"); ?>
+
+</div>
+
+<?php get_footer(); ?>
