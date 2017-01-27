04f811b88cb31223328ae2389ca1abd850504a51
Victor Andersen <v@victorb.net>
2017-01-26 23:12:59 -0800

    Featured image in background of archive pages

--- podcast-episode.php
+++ podcast-episode.php
@@ -1,8 +1,10 @@
 <?php global $multiple_on_page; ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class("block"); ?> role="article">
-        
-        <header>
-            
+    <?php if(has_post_thumbnail()): ?>
+    <header class="featured-bg" style="background-image: url(<?php echo the_post_thumbnail_url('simple_bootstrap_featured');?>);">
+    <?php else: ?>
+    <header>
+    <?php endif ?>
             <?php if ($multiple_on_page) : ?>
             <div class="article-header">
                 <h2 class="h1"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
@@ -15,16 +17,13 @@
 
             <?php if (has_post_thumbnail()) { ?>
             <div class="featured-image">
-                <?php if ($multiple_on_page) : ?>
-                <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('simple_boostrap_featured'); ?></a>
-                <?php else: ?>
+                <?php if (!$multiple_on_page) : ?>
                 <?php the_post_thumbnail('simple_boostrap_featured'); ?>
                 <?php endif ?>
             </div>
             <?php } ?>
 
             <?php include(get_stylesheet_directory() . "/podcast-meta.php"); ?>
-        
         </header>
     
         <section class="post_content">
