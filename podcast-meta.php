868f690cf5b904158d2eb9ed7aaddc6892cf77a7
Victor Andersen <v@victorb.net>
2017-01-27 00:38:09 -0800

    dashicon update

--- podcast-meta.php
+++ podcast-meta.php
@@ -1,7 +1,7 @@
 <ul class="meta text-muted list-inline">
     <?php $term_list = wp_get_post_terms( $post->ID, 'series', array( 'fields' => 'all' ) ); ?>
     <?php if( !empty($term_list) ){ ?>
-        <li>
+        <li><span class="dashicons-before dashicons-microphone"></span>
         <?php foreach( $term_list as $term ){ ?>
             <a href="<?php echo get_term_link($term); ?>"><?php echo $term->name; ?></a> 
         <?php } ?>
@@ -11,7 +11,8 @@
         <li>
             <a href="<?php the_permalink() ?>">
                 <?php if ($post->menu_order > 0): ?>
-                Episode #<?php echo $post->menu_order;?>
+                <span class="glyphicon glyphicon-link"></span>
+                Episode #<?php echo $post->menu_order;?> &nbsp;
                 <?php endif ?>
                 <span class="glyphicon glyphicon-time"></span>
                 <?php the_date(); ?>
