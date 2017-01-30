796e96b817912f3d1558c149ee68c3d0fcafe023
Victor Andersen <v@victorb.net>
2017-01-30 02:06:19 -0800

    moving sticky widgets inside content div

--- header.php
+++ header.php
@@ -60,32 +60,3 @@
 		
 		<div id="page-content">
 			<div class="container">
-            <?php if( is_active_sidebar('home_sticky_2') || is_active_sidebar('home_sticky_3') || is_active_sidebar('home_sticky_1') ): ?>
-                <div class="block sticky-widgets <?php echo simple_boostrap_main_classes(); ?>">
-                <?php if ( is_active_sidebar( 'home_sticky_1' ) ) : ?>
-                    <div class="row">
-                        <div id="sticky-sidebar-1" class="sticky-sidebar widget-area col-xs-12" role="complementary">
-                            <?php dynamic_sidebar( 'home_sticky_1' ); ?>
-                        </div><!-- #sticky-sidebar-1 -->
-                    </div>
-                <?php endif; ?>                        
-                <?php if( is_active_sidebar( 'home_sticky_2') || is_active_sidebar( 'home_sticky_3' ) ): ?>
-                    <div class="row">
-                        <div id="sticky-sidebar-2" class="sticky-sidebar widget-area col-xs-6" role="complementary">
-                            <div class="row">
-                                <?php if( is_active_sidebar( 'home_sticky_2') ): ?>
-                                <?php dynamic_sidebar( 'home_sticky_2' ); ?>
-                                <?php endif; ?>
-                            </div>
-                        </div><!-- #sticky-sidebar-2 --> 
-                        <div id="sticky-sidebar-3" class="sticky-sidebar widget-area col-xs-6" role="complementary">
-                            <div class="row">
-                                <?php if( is_active_sidebar( 'home_sticky_3') ): ?>
-                                <?php dynamic_sidebar( 'home_sticky_3' ); ?>
-                                <?php endif; ?>
-                            </div>
-                        </div><!-- #sticky-sidebar-3 --> 
-                    </div>
-                <?php endif; ?>
-                </div>
-            <?php endif; ?>
--- index.php
+++ index.php
@@ -1,7 +1,35 @@
 <?php get_header(); ?>
 
 <div id="content" class="row">
-
+    <?php if( is_active_sidebar('home_sticky_2') || is_active_sidebar('home_sticky_3') || is_active_sidebar('home_sticky_1') ): ?>
+        <div class="block sticky-widgets <?php echo simple_boostrap_main_classes(); ?>">
+        <?php if ( is_active_sidebar( 'home_sticky_1' ) ) : ?>
+            <div class="row">
+                <div id="sticky-sidebar-1" class="sticky-sidebar widget-area col-xs-12" role="complementary">
+                    <?php dynamic_sidebar( 'home_sticky_1' ); ?>
+                </div><!-- #sticky-sidebar-1 -->
+            </div>
+        <?php endif; ?>                        
+        <?php if( is_active_sidebar( 'home_sticky_2') || is_active_sidebar( 'home_sticky_3' ) ): ?>
+            <div class="row">
+                <div id="sticky-sidebar-2" class="sticky-sidebar widget-area col-xs-6" role="complementary">
+                    <div class="row">
+                        <?php if( is_active_sidebar( 'home_sticky_2') ): ?>
+                        <?php dynamic_sidebar( 'home_sticky_2' ); ?>
+                        <?php endif; ?>
+                    </div>
+                </div><!-- #sticky-sidebar-2 --> 
+                <div id="sticky-sidebar-3" class="sticky-sidebar widget-area col-xs-6" role="complementary">
+                    <div class="row">
+                        <?php if( is_active_sidebar( 'home_sticky_3') ): ?>
+                        <?php dynamic_sidebar( 'home_sticky_3' ); ?>
+                        <?php endif; ?>
+                    </div>
+                </div><!-- #sticky-sidebar-3 --> 
+            </div>
+        <?php endif; ?>
+        </div>
+    <?php endif; ?>
 	<div id="main" class="<?php simple_boostrap_main_classes(); ?>" role="main">
 
 		<?php if (have_posts()) : ?>
