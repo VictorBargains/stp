0c95940149aa73aae51aea6a7f2d9b0c07c55c57
Victor Andersen <v@victorb.net>
2017-01-30 18:42:11 -0800

    wrapping jquery

--- podcast.js
+++ podcast.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){
-    var $p_container = $('.podcast_player mejs-playpause-button');
-    $p_container.each(function(i, el){
-        var $el = $(el),
-            $clone = $el.find('button').clone(true).addClass('playpause-cloned');
-    $clone.appendTo($el.closest('article').find('header'));
-    });     
-});
+(function($) {
+    $(document).ready(function(){
+        var $p_container = $('.podcast_player mejs-playpause-button');
+        $p_container.each(function(i, el){
+            var $el = $(el),
+                $clone = $el.find('button').clone(true).addClass('playpause-cloned');
+        $clone.appendTo($el.closest('article').find('header'));
+        });     
+    });
+})(jQuery);
