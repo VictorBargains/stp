02f28c3a08466d0d4224a51b921eb841325a8f3f
Victor Andersen <v@victorb.net>
2017-01-30 18:43:15 -0800

    fix to class selector

--- podcast.js
+++ podcast.js
@@ -1,6 +1,6 @@
 (function($) {
     $(document).ready(function(){
-        var $p_container = $('.podcast_player mejs-playpause-button');
+        var $p_container = $('.podcast_player .mejs-playpause-button');
         $p_container.each(function(i, el){
             var $el = $(el),
                 $clone = $el.find('button').clone(true).addClass('playpause-cloned');
