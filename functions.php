6d2e4ae029e2ef97226c1791a0a1b82d246080f9
Victor Andersen <v@victorb.net>
2017-02-04 20:05:25 -0800

    Menu fix

--- functions.php
+++ functions.php
@@ -6,15 +6,15 @@ show custom post type in archive and term pages from https://wordpress.org/suppo
 add_filter('pre_get_posts', 'query_post_type');
 function query_post_type($query) {
 //  if(is_category() || is_tag()) {
-    if ( is_category() || is_archive() || is_tag() /*&& empty( $query->query_vars['suppress_filters'] )*/ ) {
+    if ( is_main_query() && (is_category() || is_archive() || is_tag()) /*&& empty( $query->query_vars['suppress_filters'] )*/ ) {
         $post_type = get_query_var('post_type');
         if($post_type)
-	    $post_type = $post_type;
+            $post_type = $post_type;
         else
             $post_type = array('post','podcast'); // replace cpt to your custom post type
         $query->set('post_type',$post_type);
-        return $query;
     }
+    return $query;
 }
 
 /*
