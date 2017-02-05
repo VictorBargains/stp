eaa3b4fedf4d9643d2288ac9c622a6123bb47a15
Victor Andersen <v@victorb.net>
2017-02-05 13:02:05 -0800

    Rebootstrapping and var dumping

--- taxonomy-speaker.php
+++ taxonomy-speaker.php
@@ -8,6 +8,7 @@
 			<h1 class="archive_title">
 				<?php echo get_the_archive_title() ?>
 			</h1>
+            <div class="row">
             <?php // vars
                 $queried_object = get_queried_object(); 
                 $taxonomy = $queried_object->taxonomy;
@@ -17,11 +18,12 @@
                 {
                     $meta_image = get_wp_term_image($term_id); //get category/term image url 
                     ?>
-                    <div class="speaker-photo"><img title="<?php echo $queried_object->name; ?>" src="<?php echo $meta_image; ?>">
+                    <div class="speaker-photo col-xs-4 col-sm-push-4"><img title="<?php echo $queried_object->name; ?>" src="<?php echo $meta_image; ?>">
                 <?php } ?>
 
-                    <div class="speaker-description"><p><?php echo $queried_object->description; ?></p></div>
+                    <div class="speaker-description col-sm-8 col-sm-pull-4"><p><?php echo $queried_object->description; ?></p></div>
                     
+            </div>
         </div>
 
 		<?php if (have_posts()) : ?>
@@ -45,6 +47,7 @@
 		<?php else : ?>
 		
 		<article id="post-not-found" class="block">
+            <?php var_dump($taxonomy); ?>
 		    <p><?php _e("This " . $taxonomy->singular_name . " does not appear in any episodes yet.", "simple-bootstrap"); ?></p>
 		</article>
 		
