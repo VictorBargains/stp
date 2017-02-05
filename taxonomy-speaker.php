1eb68c22a286ad6e3f0996bcaa856b09be2d4078
Victor Andersen <v@victorb.net>
2017-02-05 13:45:47 -0800

    reinstating row

--- taxonomy-speaker.php
+++ taxonomy-speaker.php
@@ -8,7 +8,7 @@
 			<h1 class="archive_title">
 				<?php echo get_the_archive_title() ?>
 			</h1>
-<!--            <div class="row">-->
+            <div class="row">
             <?php // vars
                 $queried_object = get_queried_object(); 
                 $taxonomy = get_taxonomy($queried_object->taxonomy);
@@ -25,9 +25,9 @@
                 <?php } 
                 if( !empty($queried_object->description) ){ ?>
                 
-                    <div class="speaker-description"><p><?php echo $queried_object->description; ?></p></div>
+                    <div class="speaker-description col-sm-8"><p><?php echo $queried_object->description; ?></p></div>
                 <?php } ?>
-<!--            </div>-->
+            </div>
         </div>
 
         <article id="speaker-episode-count" class="block">
