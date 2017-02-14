d2eafbbd32bf1ffc3471fe3102e22be91e09c1fc
Victor Andersen <v@victorb.net>
2017-02-13 18:16:26 -0800

    Page template should not include date, author, etc.

---
+++ page.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+
+<div id="content" class="row">
+
+	<div id="main" class="<?php simple_boostrap_main_classes(); ?>" role="main">
+
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+		<?php the_content(); ?>
+		
+		<?php endwhile; ?>		
+		
+		<?php else : ?>
+		
+		<article id="post-not-found" class="block">
+		    <p><?php _e("No pages found.", "simple-bootstrap"); ?></p>
+		</article>
+		
+		<?php endif; ?>
+
+	</div>
+
+	<?php get_sidebar("left"); ?>
+	<?php get_sidebar("right"); ?>
+
+</div>
+
+<?php get_footer(); ?>
